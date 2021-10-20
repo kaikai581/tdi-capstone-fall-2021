32aae385ce33fab99ce2ce04c5bc45331bf6b48f
Shih-Kai Lin <kaikai581@hotmail.com>
2021-10-20 17:48:58 -0500

    Create a template app.py.

---
+++ tests/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+My data dashboard for TDI's capstone project with Streamlit.
+Source reference:
+https://github.com/jkanner/streamlit-dataview/blob/master/app.py
+with the website:
+https://share.streamlit.io/jkanner/streamlit-dataview/master/app.py/+/
+'''
+
+class CapstoneDashboard:
+    def __init__(self):
+        self.main_layout()
+    
+    
